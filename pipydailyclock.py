de076759385285517d560a7f3eea994f75cbe7d5
Marc Urben <aegnor@mittelerde.ch>
2020-04-19 00:38:25 +0200

    calculating sleep time for better performance

--- pipydailyclock.py
+++ pipydailyclock.py
@@ -170,35 +170,35 @@ if __name__ == "__main__":
 
     b4_ir_init = time.time()
     ir = ImageRenderer(config)
-    logging.info("ImageRenderer took %s seconds to init" % (time.time() - b4_ir_init))
+    logging.debug("ImageRenderer took %s seconds to init" % (time.time() - b4_ir_init))
 
     if sys.argv[0] == "store":
 
         b4_init = time.time()
         ir.init_screen()
-        logging.info("init_screen took %s seconds" % (time.time() - b4_init))
+        logging.debug("init_screen took %s seconds" % (time.time() - b4_init))
 
         b4_run = time.time()
         ir.run()
-        logging.info("run took %s seconds" % (time.time() - b4_run))
+        logging.debug("run took %s seconds" % (time.time() - b4_run))
 
         b4_store = time.time()
         ir.store()
-        logging.info("store took %s seconds" % (time.time() - b4_store))
+        logging.debug("store took %s seconds" % (time.time() - b4_store))
 
     else:
         b4_init = time.time()
         ir.init_screen()
-        logging.info("init_screen took %s seconds" % (time.time() - b4_init))
+        logging.debug("init_screen took %s seconds" % (time.time() - b4_init))
         while True:
             run_start = time.time()
 
             b4_run = time.time()
             ir.run()
-            logging.info("run took %s seconds" % (time.time() - b4_run))
+            logging.debug("run took %s seconds" % (time.time() - b4_run))
 
             b4_show = time.time()
             ir.show()
-            logging.info("show took %s seconds" % (time.time() - b4_show))
+            logging.debug("show took %s seconds" % (time.time() - b4_show))
 
             time.sleep(1 - (time.time() - run_start))
